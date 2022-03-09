8cb00cc962275a147f41177acad25a3d1ca38d28
Mohamed Muntasir <devmotheg@gmail.com>
2022-03-09 09:57:50 +0300

    Add credits :]

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,15 @@
 import React from "react";
 import Project from "./components/Project";
 
-const App = () => <Project />;
+const App = () => (
+	<>
+		<Project />
+		<a
+			className="block mt-6 text-sm font-bold"
+			href="https://github.com/devmotheg">
+			Coded by Mohamed Muntasir
+		</a>
+	</>
+);
 
 export default App;
